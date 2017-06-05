39a7b397b4314af73d712a4f8a43a6fd4894798e
back99 <tofoth@naver.com>
2017-06-05 15:26:03 +0900

    1초마다 속력 거리 계산으로 바꿈

--- app/src/main/java/company/co/kr/mountainking/Fragment_mypage.java
+++ app/src/main/java/company/co/kr/mountainking/Fragment_mypage.java
@@ -81,18 +81,19 @@ public class Fragment_mypage extends Fragment {
         entries.add(new Entry(18f, 4));
         entries.add(new Entry(0f, 5));
         entries.add(new Entry(9f, 6));
+        //entries.add(new Entry(9f, 6));
 
         LineDataSet dataset = new LineDataSet(entries, "# of Calls");
 
         ArrayList<String> labels = new ArrayList<String>();
-        labels.add("1/1");
-        labels.add("1/2");
-        labels.add("1/3");
-        labels.add("1/4");
-        labels.add("1/5");
-        labels.add("1/6");
-        labels.add("1/7");
-      //  labels.add("6/25");
+        labels.add("5/29");
+        labels.add("5/30");
+        labels.add("6/1");
+        labels.add("6/2");
+        labels.add("6/3");
+        labels.add("6/4");
+        labels.add("6/5");
+      //  labels.add("6/9");
 
         LineData data = new LineData(labels, dataset);
         lineChart.setData(data);
